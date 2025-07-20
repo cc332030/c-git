a8b8aa57be95fafa589dc4ac9d4647ed302d90c2
c332030 <c332030@c332030.com>
2025-07-20 20:14:51 +0800

    feat: clone repo

--- src/Main.js
+++ src/Main.js
@@ -6,13 +6,23 @@ import {GitUtils} from './git/util/GitUtils.js'
 async function main() {
 
     const config = yaml.load(fs.readFileSync('config.yml',  'utf8'));
-    const { servers } = config
+    const { path, servers } = config
 
     // console.debug('servers', JSON.stringify(servers, null, 2));
 
     const repos = await loadRepos(servers);
 
-    fs.writeFileSync('d:/repos.txt', JSON.stringify(repos, null, 2), 'utf8');
+    // fs.writeFileSync('d:/repos.txt', JSON.stringify(repos, null, 2), 'utf8');
+
+    for (const [repo, repoConfig] of Object.entries(repos)) {
+
+        const client = repoConfig.clients[0]
+        const tokenUrl = client.getTokenUrl(repo)
+        // console.debug('tokenUrl', tokenUrl)
+
+        await GitUtils.cloneOrPull(tokenUrl, repo, path)
+
+    }
 
 }
 
--- src/git/util/GitUtils.js
+++ src/git/util/GitUtils.js
@@ -6,12 +6,25 @@
  * @since 2025/7/20
  */
 
+import fs from 'fs'
+import ora from 'ora'
 import simpleGit from 'simple-git'
+
 import {GitHubClient} from '../client/GitHubClient.js'
 import {GitLabClient} from '../client/GitLabClient.js'
 import {GITHUB, GITLAB} from '../enums/GitType.js'
 
-const git = simpleGit()
+const spinner = ora('开始下载').start()
+const progress = ({ stage, progress }) => {
+
+    let textTemplate = `当前下载进度: ${stage} ${progress}%`;
+    spinner.text = textTemplate;
+    if (progress === 100) {
+        spinner.text = textTemplate + '下载完成';
+    }
+};
+
+const git = simpleGit({progress})
 
 export class GitUtils {
 
@@ -19,27 +32,27 @@ export class GitUtils {
 
         // console.debug('dealConfig config', config)
 
-        const { host } = config;
-        let { type } = config;
+        const {host} = config
+        let {type} = config
 
-        const hostArr = host.split('://');
+        const hostArr = host.split('://')
 
         const scheme = hostArr[0]
         const domain = hostArr[1]
 
-        if(!type) {
+        if (!type) {
 
-            if(domain.indexOf(GITHUB) > -1) {
-                type = GITHUB;
-            } else if(domain.indexOf(GITLAB) > -1) {
-                type = GITLAB;
+            if (domain.indexOf(GITHUB) > -1) {
+                type = GITHUB
+            } else if (domain.indexOf(GITLAB) > -1) {
+                type = GITLAB
             } else {
-                throw new Error(`Unsupported server type ` + type);
+                throw new Error(`Unsupported server type ` + type)
             }
 
         }
 
-        return  {
+        return {
             scheme,
             domain,
             type,
@@ -49,28 +62,61 @@ export class GitUtils {
 
     static newClient(config) {
 
-        const configNew = this.dealConfig(config);
+        const configNew = this.dealConfig(config)
         // console.debug('configNew', configNew)
 
-        const { type } = configNew;
+        const {type} = configNew
 
         switch (type) {
             case GITHUB:
-                return new GitHubClient(configNew);
+                return new GitHubClient(configNew)
             case GITLAB:
-                return new GitLabClient(configNew);
+                return new GitLabClient(configNew)
             default:
-                throw new Error(`Unsupported server type ${type}`);
+                throw new Error(`Unsupported server type ${type}`)
         }
 
     }
 
-    static cloneOrPull(repoUrl, path) {
+    static async cloneOrPull(repoUrl, repo, path) {
 
-        simpleGit()
-            .clone(repoUrl,  './local-path')
-            .then(() => console.log('克隆成功'))
-            .catch(err => console.error('克隆失败:', err));
+        const repoPath = `${path}/${repo}`
+        console.debug(`\ncloneOrPull repoPath: ${repoPath}`)
+
+        try {
+
+            fs.accessSync(repoPath)
+            console.debug(`pull start`)
+
+            // 目录存在，检查是否是Git仓库
+            const gitInRepo = simpleGit(repoPath)
+            const isRepo = await gitInRepo.checkIsRepo()
+
+            if (isRepo) {
+                await gitInRepo.pull((err, result) => {
+                    if (err) {
+                        console.error(`pull failure`, err)
+                    } else {
+                        console.log(`pull success`)
+                    }
+                })
+            } else {
+                console.error(' 目标目录存在，但不是Git仓库。')
+            }
+
+        } catch (e) {
+
+            console.debug(`clone start`)
+
+            // 目录不存在，执行克隆
+            await git.clone(repoUrl, repoPath, ['--depth=1'], (cloneErr) => {
+                if (cloneErr) {
+                    console.error(`clone failure`, cloneErr)
+                } else {
+                    console.debug(`clone success`)
+                }
+            })
+        }
 
     }
 
